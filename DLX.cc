c7735c1ac2ff9195344fb2adb3a9cb9fcd8c63da
MBRSL <mabolosilugia@gmail.com>
2015-07-23 19:42:25 +0800

    Add DLX for calculating union.

---
+++ DLX.cc
@@ -0,0 +1,301 @@
+/*
+ * Original code from http://uaasoftware.com/blog/demos-tutorials/dancing-links-solving-sudoku/
+ * Modified by MBRSL
+ */
+#include <stdio.h>
+#include "DLX.hh"
+
+DLX::DLX(std::vector<FilteredTriMesh> &subset, size_t nSubMeshes)
+{
+    nRow_ = subset.size();
+    nCol_ = nSubMeshes;
+    
+    skip_counter = 0;
+    finished_ = false;
+    nMerged_subMeshes_ = 0;
+    
+    row_headers_ = new DLX_Node[nRow_];
+    col_headers_ = new DLX_Node[nCol_];
+
+    // The top most node for each column
+    DLX_Node* *top_most_nodes = new DLX_Node*[nCol_];
+    // Previous up_ node for each column
+    DLX_Node* *up_nodes = new DLX_Node*[nCol_];
+    std::fill_n(up_nodes, nCol_, nullptr);
+    std::fill_n(top_most_nodes, nCol_, nullptr);
+    
+    for (size_t j = 0; j < nRow_; j++) {
+        FilteredTriMesh &patch = subset[j];
+        size_t count = patch.merged_subMesh_idx.count();
+
+        // The left_ most node in this row
+        DLX_Node* left_most_nodes = nullptr;
+        // The previous left_ node in this row
+        DLX_Node* left_node = nullptr;
+        
+        for (size_t i = patch.merged_subMesh_idx.find_first();
+             i < nCol_ && i != patch.merged_subMesh_idx.npos;
+             i = patch.merged_subMesh_idx.find_next(i)) {
+            // create a new node
+            DLX_Node* node = new DLX_Node(DLX_Node::Node, j, i);
+            node->row_header_ = &row_headers_[j];
+            node->col_header_ = &col_headers_[i];
+            node->row_count_ = count;
+            
+            // left_ & right_
+            if (left_node != nullptr) {
+                node->left_ = left_node;
+                left_node->right_ = node;
+            } else {
+                left_most_nodes = node;
+            }
+            left_node = node;
+            
+            // up_ & down_
+            if (up_nodes[i] != nullptr) {
+                node->up_ = up_nodes[i];
+                up_nodes[i]->down_ = node;
+            } else {
+                top_most_nodes[i] = node;
+            }
+            up_nodes[i] = node;
+        }
+        
+        // Connect first & last column in this row
+        assert(left_node != nullptr);
+        left_most_nodes->left_ = left_node;
+        left_node->right_ = left_most_nodes;
+        
+        row_headers_[j].up_ = &row_headers_[(j+nRow_-1)%nRow_];
+        row_headers_[j].down_ = &row_headers_[(j+1)%nRow_];
+        row_headers_[j].left_ = left_node;
+        row_headers_[j].right_ = left_most_nodes;
+        
+        row_headers_[j].type_ = DLX_Node::Row_header;
+        row_headers_[j].row_ = j;
+        row_headers_[j].col_ = -1;
+        row_headers_[j].row_count_ = count;
+        
+        row_headers_[j].patch_ = &patch;
+    }
+    
+    // For each column, connect first & last row
+    DLX_Node* prev_left_node = nullptr;
+    for (size_t i = 0; i < nCol_; i++) {
+        // No column can be empty, otherwise no solution
+        assert(top_most_nodes[i] != nullptr && "No possible solution for this case.");
+        top_most_nodes[i]->up_ = up_nodes[i];
+        up_nodes[i]->down_ = top_most_nodes[i];
+        
+        col_headers_[i].up_ = up_nodes[i];
+        col_headers_[i].down_ = top_most_nodes[i];
+        col_headers_[i].left_ = &col_headers_[(i+nCol_-1)%nCol_];
+        col_headers_[i].right_ = &col_headers_[(i+1)%nCol_];
+
+        col_headers_[i].type_ = DLX_Node::Column_header;
+        col_headers_[i].row_ = -1;
+        col_headers_[i].col_ = i;
+        col_headers_[i].row_count_ = 0;
+    }
+    
+    delete[] up_nodes;
+    delete[] top_most_nodes;
+    
+    //Insert root
+    root_.type_ = DLX_Node::Root;
+    root_.left_ = &col_headers_[nCol_-1];
+    root_.right_ = &col_headers_[0];
+    root_.row_ = nRow_-1;
+    root_.col_ = nCol_;
+    col_headers_[0].left_ = &root_;
+    col_headers_[nCol_-1].right_ = &root_;
+}
+
+DLX::~DLX()
+{
+    for (size_t i = 0; i < nCol_; i++) {
+        for (DLX_Node* row_node = col_headers_[i].down_; row_node->down_ != col_headers_[i].down_; row_node = row_node->down_) {
+            delete row_node;
+        }
+    }
+    delete[] row_headers_;
+    delete[] col_headers_;
+}
+
+DLX_Node* DLX::choose_column(void) {
+    return root_.right_;
+}
+
+void DLX::cover(DLX_Node* col_header) {
+    col_header->right_->left_ = col_header->left_;
+    col_header->left_->right_ = col_header->right_;
+    
+//    for(DLX_Node* row_node : DLX_Iterator_Range<DLX_Down_Iterator>(col_header)) {
+    bool first_iteration = true;
+    for (DLX_Node* row_node = col_header->down_;
+         row_node != nullptr && (first_iteration || row_node != col_header->down_);
+         row_node = row_node->down_, first_iteration = false) {
+        row_node->covered = true;
+        for(DLX_Node* right_node = row_node->right_; right_node!=row_node; right_node = right_node->right_) {
+            right_node->covered = true;
+            right_node->up_->down_ = right_node->down_;
+            right_node->down_->up_ = right_node->up_;
+            
+            if (right_node->col_header_->down_ == right_node) {
+                right_node->col_header_->down_ = right_node->down_;
+            }
+            if (right_node->col_header_->up_ == right_node) {
+                right_node->col_header_->up_ = right_node->up_;
+            }
+        }
+    }
+//    print_covered();
+}
+
+void DLX::uncover(DLX_Node* col_header) {
+    bool first_iteration = true;
+    for (DLX_Node* row_node = col_header->up_;
+         row_node != nullptr && (first_iteration || row_node != col_header->up_);
+         row_node = row_node->up_, first_iteration = false) {
+        row_node->covered = false;
+        for(DLX_Node* left_node = row_node->left_; left_node!=row_node; left_node = left_node->left_) {
+            if (left_node->col_header_->down_ == left_node->down_) {
+                left_node->col_header_->down_ = left_node;
+            }
+            if (left_node->col_header_->up_ == left_node->up_) {
+                left_node->col_header_->up_ = left_node;
+            }
+
+            left_node->covered = false;
+            left_node->up_->down_ = left_node;
+            left_node->down_->up_ = left_node;
+        }
+    }
+    col_header->right_->left_ = col_header;
+    col_header->left_->right_ = col_header;
+//    print_covered();
+}
+
+void DLX::search(size_t k) {
+    typedef std::pair<std::vector<HalfedgeHandle>, double> Segment_pair;
+    auto comparator = [](Segment_pair p1, Segment_pair p2) ->bool {
+        return p1.second < p2.second;
+    };
+
+    emit setJobState((double)nMerged_subMeshes_/nCol_*100);
+    
+    if(root_.left_ == &root_ && root_.right_ == &root_) {
+        double score = 0;
+        for (DLX_Node *node : intermediate_result_) {
+            FilteredTriMesh *patch = node->patch_;
+            score += patch->seam_score;
+        }
+        
+        double min_score = !results_.empty() ? results_.top().first : 1e9;
+        if (results_.size() >= max_results && score <= min_score) {
+            skip_counter++;
+            if (skip_counter > max_skip_num) {
+                finished_ = true;
+            }
+        } else {
+            //Valid solution!
+            if (results_.size() >= max_results) {
+                results_.pop();
+            }
+            results_.push(std::make_pair(score, intermediate_result_));
+            
+            skip_counter = 0;
+            
+            print_solution();
+        }
+        emit setJobState((double)skip_counter/max_skip_num*100);
+
+        return;
+    }
+    if (k > nCol_) {
+        printf("----------- SOMETHING WRONG -----------\n");
+        finished_ = true;
+        return;
+    }
+    DLX_Node* col_header = choose_column();
+    assert(col_header->type_ != DLX_Node::Root);
+    cover(col_header);
+    
+    bool first_iteration = true;
+    for (DLX_Node* row_node = col_header->down_;
+         !finished_ &&
+         row_node != nullptr && (first_iteration || row_node != col_header->down_);
+         row_node = row_node->down_, first_iteration = false) {
+        // Try this row node on!
+        intermediate_result_.push_back(row_node->row_header_);
+        nMerged_subMeshes_ += row_node->row_count_;
+        for(DLX_Node* right_node = row_node->right_; right_node!=row_node; right_node = right_node->right_) {
+            cover(right_node->col_header_);
+        }
+        search(k+1);
+        // Ok, that node didn't quite work
+        for(DLX_Node* left_node = row_node->left_; left_node!=row_node; left_node = left_node->left_) {
+            uncover(left_node->col_header_);
+        }
+        intermediate_result_.pop_back();
+        nMerged_subMeshes_ -= row_node->row_count_;
+    }
+    
+    uncover(col_header);
+}
+
+void DLX::print_covered() {
+    for (size_t j = 0; j < nRow_; j++) {
+        bool first_iteration = true;
+        int last_col = 0;
+        for(DLX_Node* right_node = row_headers_[j].right_;
+            right_node != nullptr && (first_iteration || right_node!=row_headers_[j].right_);
+            right_node = right_node->right_, first_iteration = false) {
+            
+            if (!right_node->covered) {
+                for (int a = last_col; a < right_node->col_; a++) {
+                    printf("   ");
+                }
+                printf("%3d", right_node->col_);
+                last_col = right_node->col_+1;
+            }
+        }
+        if (last_col != 0) {
+            printf("\n");
+        }
+    }
+    printf("-------------------------\n");
+}
+
+void DLX::print_solution(void) {
+#ifndef NDEBUG
+    printf("----------- SOLUTION FOUND -----------\n");
+    for(DLX_Node *row_header : intermediate_result_) {
+        printf("Row %d:", row_header->row_);
+        bool first_iteration = true;
+        for (DLX_Node* right_node = row_header->right_;
+             right_node != nullptr && (first_iteration || right_node != row_header->right_);
+             right_node = right_node->right_, first_iteration = false) {
+            printf("%d ", right_node->col_);
+        }
+        printf("\n");
+    }
+    printf("---------------------\n");
+#endif
+}
+
+std::vector< std::vector<int> > DLX::get_results() {
+    std::vector< std::vector<int> > results;
+    while (!results_.empty()) {
+        auto &pair = results_.top();
+        
+        std::vector<int> list;
+        for (DLX_Node* node : pair.second) {
+            list.push_back(node->row_);
+        }
+        results.push_back(list);
+        
+        results_.pop();
+    }
+    return results;
+}
---
+++ DLX.hh
@@ -0,0 +1,182 @@
+/*
+ * Original code from http://uaasoftware.com/blog/demos-tutorials/dancing-links-solving-sudoku/
+ */
+#include "FilteredTriMesh.hh"
+
+#include <queue>
+
+#include <QObject>
+
+class DLX_Node {
+public:
+    DLX_Node* row_header_;
+    DLX_Node* col_header_;
+    
+    DLX_Node* left_;
+    DLX_Node* right_;
+    DLX_Node* up_;
+    DLX_Node* down_;
+
+    enum Type {Root, Row_header, Column_header, Node} type_;
+    int row_;
+    int col_;
+    size_t row_count_;
+    
+    bool covered = false;
+    
+    FilteredTriMesh* patch_;
+    
+    DLX_Node() :
+    DLX_Node(Node, 0, 0)
+    {
+    }
+    DLX_Node(Type type, int row, int col) :
+    row_header_(nullptr),
+    col_header_(nullptr),
+    left_(nullptr),
+    right_(nullptr),
+    up_(nullptr),
+    down_(nullptr),
+    type_(type),
+    row_(row),
+    col_(col),
+    row_count_(0),
+    patch_(nullptr)
+    {
+    }
+};
+
+//template <class Derived>
+//class DLX_Iterator {
+//public:
+//    DLX_Iterator(DLX_Node *node) :
+//    current_node_(node), is_first_iteration_(true) {}
+//
+//    DLX_Node* operator*() const {
+//        return current_node_;
+//    }
+//    template <class Iter>
+//    bool operator==(const Iter &other) const {
+//        return !is_first_iteration_ && this->operator*() == *other;
+//    }
+//    template <class Iter>
+//    bool operator!=(const Iter &other) const {
+//        return is_first_iteration_ || this->operator*() != *other;
+//    }
+//    Derived& operator++() {
+//        return static_cast<const Derived*>(this)->operator++();
+//    }
+//protected:
+//    DLX_Node *current_node_;
+//    bool is_first_iteration_;
+//};
+//
+//class DLX_Down_Iterator : public DLX_Iterator<DLX_Down_Iterator> {
+//public:
+//    DLX_Down_Iterator(DLX_Node *header) : DLX_Iterator (header->down_) {}
+//    DLX_Down_Iterator& operator++() {
+//        is_first_iteration_ = false;
+//        current_node_ = current_node_->down_;
+//        return *this;
+//    }
+//};
+//
+//class DLX_Up_Iterator : public DLX_Iterator<DLX_Up_Iterator> {
+//public:
+//    DLX_Up_Iterator(DLX_Node *header) : DLX_Iterator (header->up_) {}
+//    DLX_Up_Iterator& operator++() {
+//        is_first_iteration_ = false;
+//        current_node_ = current_node_->up_;
+//        return *this;
+//    }
+//};
+//
+//class DLX_Right_Iterator : public DLX_Iterator<DLX_Right_Iterator> {
+//public:
+//    DLX_Right_Iterator(DLX_Node *header) : DLX_Iterator (header->right_) {}
+//    DLX_Right_Iterator& operator++() {
+//        is_first_iteration_ = false;
+//        current_node_ = current_node_->right_;
+//        return *this;
+//    }
+//};
+//
+//class DLX_Left_Iterator : public DLX_Iterator<DLX_Left_Iterator> {
+//public:
+//    DLX_Left_Iterator(DLX_Node *header) : DLX_Iterator (header->left_) {}
+//    DLX_Left_Iterator& operator++() {
+//        is_first_iteration_ = false;
+//        current_node_ = current_node_->left_;
+//        return *this;
+//    }
+//};
+//
+//template <class Iter>
+//class DLX_Iterator_Range {
+//public:
+//    Iter begin() {
+//        return Iter(node_);
+//    }
+//    Iter end() {
+//        return Iter(node_);
+//    }
+//    
+//    DLX_Iterator_Range(DLX_Node *node) : node_(node) {}
+//    
+//private:
+//    DLX_Node *node_;
+//};
+
+class DLX : public QObject
+{
+    Q_OBJECT
+signals:
+    void setJobState(int val);
+
+private:
+    size_t nRow_;
+    size_t nCol_;
+    
+    /// maximum results recorded
+    const static size_t max_results = 20;
+    /// +1 if the result is not better than previous result for each calculation
+    size_t skip_counter;
+    /// The search terminates if skip counter reach this number
+    const static size_t max_skip_num = 100000;
+    
+    /*
+     * These are the header nodes of each row/column.
+     * They are not treated as part of the DLX, they just point to the starting/ending nodes.
+     * The up/down/left/right pointer of inner nodes will not point to these headers.
+     */
+    DLX_Node *row_headers_;
+    DLX_Node *col_headers_;
+    /*
+     * Can be treated as an entrance point of DLX.
+     * Only point to the first/last column headers, not part of DLX.
+     */
+    DLX_Node root_;
+    
+    std::priority_queue<    std::pair< double, std::vector<DLX_Node*> >,
+                            std::vector< std::pair< double, std::vector<DLX_Node*> > >,
+                            std::greater< std::pair< double, std::vector<DLX_Node*> > > > results_;
+
+    std::vector<DLX_Node*> intermediate_result_;
+    size_t nMerged_subMeshes_;
+    bool finished_;
+    
+public:
+    DLX(std::vector<FilteredTriMesh> &subset, size_t nSubMeshes);
+    ~DLX();
+    
+    // --> DLX Algorithm functions
+    DLX_Node *choose_column(void);
+    void cover(DLX_Node* col_header);
+    void uncover(DLX_Node* col_header);
+    void print_solution(void);
+    void search(size_t k);
+    
+    void print_covered();
+    
+    std::vector< std::vector<int> > get_results();
+};
